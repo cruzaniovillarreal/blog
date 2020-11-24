fcce7e2f33ecd52f913fd927e2478b36404ca26c
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-11-24 09:12:44 -0600

    created initial spring

---
+++ src/main/java/com/codeup/initalspring/InitalspringApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.initalspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitalspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitalspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/initalspring/InitalspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.initalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitalspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
