1c991e1ea474b69a1e5bf79ae92a9d21a07b6b91
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-11-24 09:34:27 -0600

    created Math and Home Controllers to handed landing page message and basic arithmetic operations

---
+++ src/main/java/com/codeup/initalspring/HomeController.java
@@ -0,0 +1,18 @@
+package com.codeup.initalspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String landingPage() {
+        return "This is the landing page!";
+    };
+
+
+
+}
---
+++ src/main/java/com/codeup/initalspring/MathController.java
@@ -0,0 +1,34 @@
+package com.codeup.initalspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+    @GetMapping("/add/{num}/and/{num2}")
+    @ResponseBody
+    public int addition(@PathVariable int num, @PathVariable int num2) {
+        return num + num2;
+    };
+
+    @GetMapping("/subtract/{num}/from/{num2}")
+    @ResponseBody
+    public int subtraction(@PathVariable int num, @PathVariable int num2) {
+        return num - num2;
+    };
+
+    @GetMapping("/multiply/{num}/and/{num2}")
+    @ResponseBody
+    public int multiply(@PathVariable int num, @PathVariable int num2) {
+        return num * num2;
+    };
+
+    @GetMapping("/divide/{num}/by/{num2}")
+    @ResponseBody
+    public int divide(@PathVariable int num, @PathVariable int num2) {
+        return num/num2;
+    };
+
+}
